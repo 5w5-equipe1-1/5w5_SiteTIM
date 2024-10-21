f49c66bd9cd3a1a4163aa7464b05e2974eba4a5a
Yannick-btn <143724027+Yannick-btn@users.noreply.github.com>
2024-10-20 22:28:41 -0400

    ajout d'une animation de scroll orizontale de la banniere

--- themes/TIM/js/animations.js
+++ themes/TIM/js/animations.js
@@ -23,25 +23,55 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-//Parallax
-let fenceGauche = document.querySelector('.fence-gauche');
-let fencedroite = document.querySelector('.fence-droite');
+// //Parallax
+// let fenceGauche = document.querySelector('.fence-gauche');
+// let fencedroite = document.querySelector('.fence-droite');
 
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
+// window.addEventListener('scroll', () => {
+//     let value = window.scrollY;
 
-    // Ajuste la translation pour donner un effet de mouvement
-    fenceGauche.style.transform = `translateX(${value * 0.4}px)`; 
-    fencedroite.style.transform = `translateX(${value * -0.4}px)`;
+//     // Ajuste la translation pour donner un effet de mouvement
+//     fenceGauche.style.transform = `translateX(${value * 0.4}px)`; 
+//     fencedroite.style.transform = `translateX(${value * -0.4}px)`;
+// });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Scroll in view
+    let scrollElement = document.querySelector('.text_cercle');
+
+    if (scrollElement) {
+        scrollElement.addEventListener('click', () => {
+            window.scrollTo({
+                top: 800,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    // Scroll animation
+    let scrollAnim = document.querySelector('.bannieres');
+
+    if (scrollAnim) {
+        window.addEventListener('scroll', () => {
+            let value = window.scrollY;
+            let elementTop = scrollAnim.getBoundingClientRect().top + window.scrollY;
+            let windowHeight = window.innerHeight;
+
+            // Calculer le pourcentage de défilement par rapport à la hauteur de la fenêtre
+            let scrollPercentage = ((value + windowHeight - elementTop) / windowHeight) * 100;
+
+            // Limiter le pourcentage de défilement entre 0 et 100
+            scrollPercentage = Math.min(Math.max(scrollPercentage, 0), 100);
+
+            // Calculer la nouvelle position en pourcentage
+            let newPosition = 50 - (scrollPercentage * 0.5);
+
+            // Appliquer la nouvelle position
+            scrollAnim.style.transform = `translateX(${-newPosition}%)`;
+        });
+    }
 });
 
 
-//scroll in view
-let scrollElement = document.querySelector('.text_cercle');
 
-scrollElement.addEventListener('click', () => {
-    window.scrollTo({
-        top: 800,
-        behavior: 'smooth'
-    });
-});
