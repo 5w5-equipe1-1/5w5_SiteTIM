2a76e4c93bd6740969d98242262b17a8ceff900e
el0ooo <eloichayer@gmail.com>
2024-11-14 15:41:45 -0500

    mise en place de la page 404 en php

--- themes/TIM/404.php
+++ themes/TIM/404.php
@@ -90,23 +90,8 @@
         <script src="https://kit.fontawesome.com/a189675535.js" crossorigin="anonymous"></script>
         <!-- Script pour les animations de scroll -->
         <script src="<?php echo get_template_directory_uri() ."/js/animations.js"?>"></script>
-        <!-- Script pour le menu burger -->
-        <script src="<?php echo get_template_directory_uri() ."/js/cubeBergur.js"?>"></script>
-        <!-- inclure le script qui gere le changement de page -->
-        <script src="<?php echo get_template_directory_uri() . "/js/selectionCat.js"?>"></script>
-        <!-- inclure le script pour le carrousel -->
-        <script src="<?php echo get_template_directory_uri() . "/js/carrousel.js"?>"></script>
-    </body>
-</html>
-<?php 
-    get_footer(); // Afficher le footer
-?>
-        <!-- API fontawsome pour les icones -->
-        <script src="https://kit.fontawesome.com/a189675535.js" crossorigin="anonymous"></script>
-        <!-- Script pour les animations de scroll -->
-        <script src="<?php echo get_template_directory_uri() ."/js/animations.js"?>"></script>
-        <!-- Script pour le menu burger -->
-        <script src="<?php echo get_template_directory_uri() ."/js/cubeBergur.js"?>"></script>
+        <!-- inclure le script qui gere le cube 3D -->
+        <script src="<?php echo get_template_directory_uri() . "/js/cube.js"?>"></script>
         <!-- inclure le script qui gere le changement de page -->
         <script src="<?php echo get_template_directory_uri() . "/js/selectionCat.js"?>"></script>
         <!-- inclure le script pour le carrousel -->
