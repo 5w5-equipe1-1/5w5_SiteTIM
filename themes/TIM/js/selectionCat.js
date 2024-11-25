41c5d13cdf2de1b52d20d2cff62080a088521026
Mokraoui <e2265448@cmaisonneuve.qc.ca>
2024-11-25 14:22:28 -0500

    correction du main dans search.php

--- themes/TIM/js/selectionCat.js
+++ themes/TIM/js/selectionCat.js
@@ -23,10 +23,10 @@
 
       // Construire l'URL avec l'ID de la catégorie
       //url yannick
-      let url = `http://localhost/5w5/wp-content/themes/TIM/categories.php?category=${cat}`;
+      //let url = `http://localhost/5w5/wp-content/themes/TIM/categories.php?category=${cat}`;
       //let url = `http://localhost/5w5gabrielle/wp-content/themes/TIM/categories.php?category=${cat}`;
       //let url = `https://gftnth00.mywhc.ca/tim23/wp-content/themes/TIM/categories.php?category=${cat}`;
-      //let url = `http://localhost:8080/5w5_syphax/wp-content/themes/TIM/categories.php?category=${cat}`;
+      let url = `http://localhost:8080/5w5/wp-content/themes/TIM/categories.php?category=${cat}`;
       // Rediriger l'utilisateur vers la nouvelle page
       window.location.href = url;
     });
@@ -52,10 +52,10 @@
 
       // Construire l'URL avec l'ID de la catégorie
       //url yannick
-      let url = `http://localhost/5w5/wp-content/themes/TIM/categories.php?category=${cat}`;
+      //let url = `http://localhost/5w5/wp-content/themes/TIM/categories.php?category=${cat}`;
       //let url = `http://localhost/5w5gabrielle/wp-content/themes/TIM/categories.php?category=${cat}`;
       //let url = `https://gftnth00.mywhc.ca/tim23/wp-content/themes/TIM/categories.php?category=${cat}`;
-      //let url = `http://localhost:8080/5w5_syphax/wp-content/themes/TIM/categories.php?category=${cat}`;
+      let url = `http://localhost:8080/5w5/wp-content/themes/TIM/categories.php?category=${cat}`;
       // Rediriger l'utilisateur vers la nouvelle page
       window.location.href = url;
     });
